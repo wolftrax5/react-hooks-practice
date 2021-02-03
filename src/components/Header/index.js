0f68d493a46cee1b9591887f3949dfe3574c5b71
Alejandro Medina <5almero5@gmail.com>
2021-02-03 15:01:49 -0600

    Add theme context

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import ThemeContext from '../../context/ThemeContext';
+
 
 export const Header = () => {
     const [darkmode, setDarckMode] = useState(false);
+    const color = useContext(ThemeContext);
 
     const handleClick = () => {
         setDarckMode(!darkmode);
@@ -9,7 +12,7 @@ export const Header = () => {
 
     return(
         <div className='Header'>
-            <h1>React Hooks</h1>
+            <h1 style={ {color} }>React Hooks</h1>
             <button type='button' onClick={handleClick}>{darkmode ? 'Dark Mode' : 'Ligth Mode'}</button>
             <button type='button' onClick={()=> setDarckMode(!darkmode)}>{darkmode ? 'Dark Mode 2' : 'Ligth Mode 2'}</button>
         </div>
