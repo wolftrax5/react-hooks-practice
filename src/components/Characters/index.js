ce5ca695d2b2f68f36bf5b4ce527860ea2ea969c
Alejandro Medina <5almero5@gmail.com>
2021-02-04 18:05:50 -0600

    Add useReducer state

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,8 +1,25 @@
-import React , { useState, useEffect } from 'react'
+import React , { useState, useEffect, useReducer } from 'react'
 
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites:[...state.favorites, action.payload]
+            };
+            default:
+                return state;
+    }
+}
 
 export const Characters = () => {
     const [characters, setCharacters] = useState([])
+
+    const [reducerState, dispatch] = useReducer(favoriteReducer, initialState);
 
     useEffect(()=> {
         fetch('https://swapi.dev/api/people/')
@@ -10,9 +27,26 @@ export const Characters = () => {
             .then(data => setCharacters(data.results))
     }, [])
 
+    const handlerClick = (favorite) => {
+        dispatch({type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
     return(
         <div className='characters'>
-        {characters.map((character, i) => <h2 key={i}>{character.name}</h2>)}
+        {reducerState.favorites.map(fav => (
+            <li key={fav.created}>
+                    {fav.name}
+            </li>
+        ))}
+        {characters.map((character) => (
+            <div key={character.name} 
+                 className='characters__detail'>
+                <h2 >{character.name}</h2>
+                <button 
+                    type='button'
+                    onClick={()=>handlerClick(character)}>Add to Fav</button>
+            </div>
+        ))}
         </div>
     )
 }
