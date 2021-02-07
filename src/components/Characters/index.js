fed337b7688c8a039f5a5bae9de8b7763c0763d5
Alejandro Medina <5almero5@gmail.com>
2021-02-06 18:41:37 -0600

    Add  Use of Marvel API

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,5 +1,12 @@
 import React , { useState, useEffect, useReducer } from 'react'
+import md5 from 'md5';
 
+const timestamp = '1';
+const PrivateKey = process.env.REACT_APP_API_MARVEL_KEY_PRIVATE
+const PublicKey = process.env.REACT_APP_API_MARVEL_KEY_PUBLIC
+const temphas= `${timestamp}${PrivateKey}${PublicKey}`
+ // md5(ts+privateKey+publicKey)
+const Apihash = md5(temphas)
 const initialState = {
     favorites: []
 }
@@ -20,11 +27,13 @@ export const Characters = () => {
     const [characters, setCharacters] = useState([])
 
     const [reducerState, dispatch] = useReducer(favoriteReducer, initialState);
-
+   
     useEffect(()=> {
-        fetch('https://swapi.dev/api/people/')
+        console.log(PrivateKey)
+        console.log(PublicKey)
+        fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${PublicKey}&hash=${Apihash}`)
             .then(response => response.json())
-            .then(data => setCharacters(data.results))
+            .then(response => setCharacters(response.data.results))
     }, [])
 
     const handlerClick = (favorite) => {
@@ -33,13 +42,13 @@ export const Characters = () => {
 
     return(
         <div className='characters'>
-        {reducerState.favorites.map(fav => (
-            <li key={fav.created}>
+        {reducerState.favorites.map((fav) => (
+            <li key={fav.id+fav.name}>
                     {fav.name}
             </li>
         ))}
         {characters.map((character) => (
-            <div key={character.name} 
+            <div key={character.id} 
                  className='characters__detail'>
                 <h2 >{character.name}</h2>
                 <button 
